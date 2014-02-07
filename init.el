ddbb48be1e18ed39055b1b7f8b6e572dddc4eb36
Reggies <kernelalex@yandex.ru>
2014-02-07 22:37:45 +0000

    Added C++11 keywords.

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 
               ;; It should be done together
               initial-frame-alist '((cursor-type . box))
-              blink-cursor-alist '((box . hollow))              
+              blink-cursor-alist '((box . hollow))
               
               ;; awk for all
               c-basic-offset 4
@@ -114,3 +114,23 @@
                '(statement-cont . align-enum-class-closing-brace)))
 
 (add-hook 'c++-mode-hook 'fix-enum-class)
+
+;; Fixin some c++11 keywords as in http://stackoverflow.com/a/17087959
+(add-hook
+ 'c++-mode-hook
+ '(lambda()
+    (font-lock-add-keywords
+     nil '(("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+           ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
+
+           ;; PREPROCESSOR_CONSTANT, PREPROCESSORCONSTANT
+           ("\\<[A-Z]*_[A-Z_]+\\>" . font-lock-constant-face)
+           ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face)
+
+           ;; hexadecimal numbers
+           ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+
+           ;; integer/float/scientific numbers
+           ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+           ))
+    ) t)
