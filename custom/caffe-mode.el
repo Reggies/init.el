5471fa96d8dd3cf4f3ccab692af6aa4fd14f85fd
Alexey Natalin <mrreggies@gmail.com>
2016-06-07 17:43:59 +0600

    Improve function highlight

--- custom/caffe-mode.el
+++ custom/caffe-mode.el
@@ -67,7 +67,7 @@
 
 (defvar caffe-font-lock-keywords
   '(("#.*" . font-lock-comment-face)
-    ("^[ \t]*\\(\\w+\\)[ \t]*{" . font-lock-function-name-face)
+    ("\\<\\w+\\>[ \t\n]*{" . font-lock-function-name-face)
     ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([eE][-+]?[0-9]+\\)?\\f?\\>" . font-lock-constant-face)
     ("\\<\\(false\\|true\\)\\>" . font-lock-constant-face)
     ("\\<\\([A-Z]\\w*\\)\\>" . font-lock-constant-face)
