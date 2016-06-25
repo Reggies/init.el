486afafefd583c3a426dd118ebcabe2818fbaaa1
Alexey Natalin <mrreggies@gmail.com>
2016-06-25 21:09:33 +0600

    Fix bugyg function face

--- custom/caffe-mode.el
+++ custom/caffe-mode.el
@@ -67,7 +67,8 @@
 
 (defvar caffe-font-lock-keywords
   '(("#.*" . font-lock-comment-face)
-    ("\\<\\w+\\>[ \t\n]*{" . font-lock-function-name-face)
+    ("\\(\\<\\w+\\>\\)[ \t\n]*{"
+     (1 font-lock-function-name-face nil t))
     ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([eE][-+]?[0-9]+\\)?\\f?\\>" . font-lock-constant-face)
     ("\\<\\(false\\|true\\)\\>" . font-lock-constant-face)
     ("\\<\\([A-Z]\\w*\\)\\>" . font-lock-constant-face)
