2f20869118776fe18d2ab1d02ede832abe7100c3
Alexey Natalin <mrreggies@gmail.com>
2017-04-08 00:00:53 +0700

    Disable menu bar

--- init.el
+++ init.el
@@ -44,6 +44,8 @@
     (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
 
 (add-to-list 'load-path (concat user-emacs-directory "custom"))
 
