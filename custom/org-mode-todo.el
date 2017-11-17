c0b1568ebc4940eed8e72fd6f424aa3cf9f141b1
Alexey Natalin <mrreggies@gmail.com>
2017-11-17 16:55:14 +0700

    fixed keyword sequence and appearance

--- custom/org-mode-todo.el
+++ custom/org-mode-todo.el
@@ -2,8 +2,8 @@
 (add-to-list 'auto-mode-alist '("TODO\\'" . org-mode))
 
 (setq-default org-todo-keyword-faces
-              '(("WONTFIX" . (:foreground "gray" :weight bold))
-                ("FAIL" . (:foreground "red" :weight bold))))
+              '(("WONTFIX" . (:foreground "gray" :weight bold :box t))
+                ("FAIL" . (:foreground "red" :weight bold :box t))))
 
 (setq-default org-todo-keywords
-              '((sequence "TODO" "DONE" "FAIL" "WONTFIX")))
+              '((sequence "TODO" "WONTFIX" "FAIL" "DONE")))
