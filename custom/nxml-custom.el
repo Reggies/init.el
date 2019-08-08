a1427a0b1f94d684a090214ab9fb5b92826aef1b
Alexey Natalin <mrreggies@gmail.com>
2019-08-08 15:55:09 +0700

    nxml: get rid of old stuff

--- custom/nxml-custom.el
+++ custom/nxml-custom.el
@@ -1,12 +1,6 @@
-(defun local-sgml-mode-hook
-  (setq fill-column 80
-        indent-tabs-mode nil
-        next-line-add-newlines nil
-        sgml-indent-data t)
-  (auto-fill-mode t)
-  (setq indent-line-function 'insert-tab)
-  (setq nxml-child-indent 4
-        nxml-attribute-indent 4
-        nxml-slash-auto-complete-flag t))
-(add-hook 'psgml-mode-hook '(lambda () (local-psgml-mode-hook)))
+(defun my-nxml-mode-hook ()
+    (setq nxml-child-indent 4
+          nxml-attribute-indent 4
+          nxml-slash-auto-complete-flag t))
+(add-hook 'nxml-mode-hook '(lambda () (my-nxml-mode-hook)))
 (provide 'nxml-custom)
